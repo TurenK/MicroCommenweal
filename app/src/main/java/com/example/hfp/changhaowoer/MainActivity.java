829d4c900a7c1336faf35b6df79519b12d006900
changhao <1037855969@qq.com>
2018-04-25 19:39:45 +0800

    login and register

--- app/src/main/java/com/example/hfp/changhaowoer/MainActivity.java
+++ app/src/main/java/com/example/hfp/changhaowoer/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.hfp.changhaowoer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView reg_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reg_text = (TextView)findViewById(R.id.reg_text);
+        reg_text.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.reg_text:
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+                //切换界面效果
+                overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
+                break;
+        }
+
+
     }
 }
