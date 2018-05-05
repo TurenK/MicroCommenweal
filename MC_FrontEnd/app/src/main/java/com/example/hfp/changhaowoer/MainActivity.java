5a0a1772dea61467c0331b8cab2991c49e62ea21
perbeuo <perbeuo@outlook.com>
2018-05-05 13:02:52 +0800

    basic json parser & asynchttp

--- MC_FrontEnd/app/src/main/java/com/example/hfp/changhaowoer/MainActivity.java
+++ MC_FrontEnd/app/src/main/java/com/example/hfp/changhaowoer/MainActivity.java
@@ -3,19 +3,32 @@ package com.example.hfp.changhaowoer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.hfp.changhaowoer.object.Charity;
+import com.alibaba.fastjson.JSONObject;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView reg_text;
     private Button login;
+    private EditText username;
+    private  EditText pwd;
+    private static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         reg_text = (TextView)findViewById(R.id.reg_text);
         login = (Button)findViewById(R.id.loginbtn);
+        username = findViewById(R.id.username);
+        pwd = findViewById(R.id.password);
         reg_text.setOnClickListener(this);
         login.setOnClickListener(this);
     }
@@ -31,11 +44,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.loginbtn:
+                login();
                 startActivity(new Intent(MainActivity.this,MainUIActivity.class));
                 finish();
                 break;
         }
 
-
     }
+
+    private boolean login(){
+        String s_username = username.getText().toString().trim();
+        String s_pwd = pwd.getText().toString();
+        String url = "";
+//        String info = "{ \"userName\":\"" + s_username + "\" , \"Password\":\"" + s_pwd + "\" }";
+//        JSONObject jsonObject = JSONObject.parseObject(info);
+
+        //创建网络访问对象
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("userName", s_username);
+        params.put("Password", s_pwd);
+
+        asyncHttpClient.post(url, params, new AsyncHttpResponseHandler(){
+            @Override
+            public void onSuccess(String content) {
+                JSONObject jsonObject = JSONObject.parseObject(content);
+                int code = jsonObject.getInteger("code");
+                String info = jsonObject.getString("message");
+                if (code == 200){
+                 Log.d(TAG, info);
+                    JSONObject object = jsonObject.getJSONObject("data");
+                    String uId = object.getString("userId");
+                    String uName = object.getString("userName");
+                    String uPhone = object.getString("userTelephone");
+                    String uAge = object.getString("userAge");
+                    String uAvatar = object.getString("userImage");
+                    String uLable = object.getString("userLabel");
+                    String uAttention = object.getString("userAttention");
+                }else if(code == 400){
+
+                }
+                super.onSuccess(content);
+            }
+
+            @Override
+            public void onFailure(Throwable error, String content) {
+                super.onFailure(error, content);
+            }
+        });
+
+        return true;
+    }
+
+
 }
