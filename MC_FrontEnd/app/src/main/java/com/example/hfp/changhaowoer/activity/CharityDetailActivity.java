d739a90537164bb0afa7fda48a7fa8f7f90ae94e
changhao <1037855969@qq.com>
2018-05-08 21:10:43 +0800

    ChrityDetail

---
+++ MC_FrontEnd/app/src/main/java/com/example/hfp/changhaowoer/activity/CharityDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.hfp.changhaowoer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hfp.changhaowoer.R;
+
+public class CharityDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity_detail);
+    }
+}
