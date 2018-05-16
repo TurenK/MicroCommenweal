b8c7a994b2761f712f765010568393b6c19a7216
changhao <1037855969@qq.com>
2018-05-16 17:47:57 +0800

    change publish

--- MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/activity/PublishActivity.java
+++ MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/activity/PublishActivity.java
@@ -10,22 +10,40 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.hfp.MicroCommonweal.R;
 
-public class PublishActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PublishActivity extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemSelectedListener {
 
     private static final int IMAGE_REQUEST_CODE = 0;
 
     private String pic_path;
-
+    private List<String> list;
+    private ArrayAdapter<String> adapter;
+    private String charity_category;
 
     private Button selectpic;
+    private Spinner spDown;
     private ImageView charity_iamge;
+    private EditText et_title;
+    private EditText et_people_num;
+    private EditText et_begin;
+    private EditText et_end;
+    private EditText et_position;
+    private EditText et_phone;
+    private EditText et_detail;
 
 
     @Override
@@ -34,8 +52,39 @@ public class PublishActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_publish);
 
         selectpic = (Button)findViewById(R.id.select_pic);
+        spDown=(Spinner) findViewById(R.id.et_charity_category);
+        et_title = (EditText)findViewById(R.id.et_title);
+        et_people_num = (EditText)findViewById(R.id.et_people_num);
+        et_begin = (EditText)findViewById(R.id.et_begin);
+        et_end = (EditText)findViewById(R.id.et_end);
+        et_position = (EditText)findViewById(R.id.et_position);
+        et_phone = (EditText)findViewById(R.id.et_phone);
+        et_detail = (EditText)findViewById(R.id.et_detail);
         charity_iamge = (ImageView)findViewById(R.id.charity_iamge);
         selectpic.setOnClickListener(this);
+
+
+
+
+        /*设置数据源*/
+        list=new ArrayList<String>();
+        list.add("青少年活动");
+        list.add("青少年活动");
+        list.add("青少年活动");
+        list.add("青少年活动");
+
+
+        /*新建适配器*/
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+
+        /*adapter设置一个下拉列表样式，参数为系统子布局*/
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        /*spDown加载适配器*/
+        spDown.setAdapter(adapter);
+
+        /*soDown的监听器*/
+        spDown.setOnItemSelectedListener(this);
     }
 
 
@@ -52,15 +101,22 @@ public class PublishActivity extends AppCompatActivity implements View.OnClickLi
     }
 
 
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (data != null) {
-//            if (requestCode == ActivityRequestCode.SHOW_MAP_DEPOT
-//                    && resultCode == Activity.RESULT_OK)
-//                showYourPic(data);
-//        }
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_message, menu);
+        return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+         charity_category=adapter.getItem(position);   //获取选中的那一项
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
