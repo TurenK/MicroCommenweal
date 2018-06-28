9db239ca24fdc3f49ee08c5ad815159508680b92
changhao <1037855969@qq.com>
2018-06-28 17:00:42 +0800

    答题activities

---
+++ MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/activity/QuestionChoseActivity.java
@@ -0,0 +1,15 @@
+package com.example.hfp.MicroCommonweal.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hfp.MicroCommonweal.R;
+
+public class QuestionChoseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_chose);
+    }
+}
