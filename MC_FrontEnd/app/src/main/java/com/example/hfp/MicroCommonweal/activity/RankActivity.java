09214af5c3e38a9e8bdd4d3dbf26a125bef9a73a
TurenK <118773103@qq.com>
2018-06-29 15:39:19 +0800

    debug

--- MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/activity/RankActivity.java
+++ MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/activity/RankActivity.java
@@ -1,23 +1,31 @@
 package com.example.hfp.MicroCommonweal.activity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.hfp.MicroCommonweal.R;
+import com.example.hfp.MicroCommonweal.adapter.PerCommitAdapter;
 import com.example.hfp.MicroCommonweal.adapter.RankAdapter;
 import com.example.hfp.MicroCommonweal.object.Rank;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RankActivity extends AppCompatActivity implements View.OnClickListener{
+public class RankActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener,View.OnClickListener{
     private List<Rank> rankList = new ArrayList<>();
     RecyclerView recyclerView;
     private Button button_back;
+    private RankAdapter adapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +34,55 @@ public class RankActivity extends AppCompatActivity implements View.OnClickListe
         button_back = (Button)findViewById(R.id.button_back);
         button_back.setOnClickListener(this);
 
+        initView();
 
         initRank();//初始化消息
-        //初始化消息列表的recycle和adapter
+    }
+
+    private void initView() {
         recyclerView = (RecyclerView)findViewById(R.id.rv_rank);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        RankAdapter adapter = new RankAdapter(rankList);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        initAdapter();
+        // addHeadView();
         recyclerView.setAdapter(adapter);
     }
 
+    /**
+     * 刷新listView
+     */
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initRank();
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        }, 1000);
+    }
+
+    /**
+     * 初始化adapter
+     */
+    private void initAdapter() {
+        adapter = new RankAdapter(R.layout.rank_item, rankList,this);
+        adapter.openLoadAnimation();
+        recyclerView.setAdapter(adapter);
+        //  addHeadView();
+        //item添加监听
+//        adapter.setOnRecyclerViewItemClickListener(new BaseQuickAdapter.OnRecyclerViewItemClickListener() {
+//            @Override
+//            public void onItemClick(View view, int position) {
+//                Toast.makeText(OrgRateActivity.this, Integer.toString(position), Toast.LENGTH_LONG).show();
+//                Intent intent=new Intent();
+//                intent.setClass(OrgRateActivity.this, CommitPerActivity.class);
+//                intent.putExtra("activityID", charityList.get(position).getaID());
+//                startActivity(intent);
+//            }
+//        });
+    }
 
     @Override
     public void onClick(View v) {
@@ -49,14 +96,15 @@ public class RankActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private  void initRank(){
+        List<Rank> ranks = new ArrayList<>();
         for(int i =0;i<10;i++){
             Rank rank = new Rank();
             rank.setAvator(R.drawable.crown_avatar_normal);
             rank.setDonatenumber("1000");
             rank.setRank(String.valueOf(i+1));
-            rankList.add(rank);
+            ranks.add(rank);
         }
+        adapter.removeAllData();
+        adapter.addData(ranks);
     }
-
-
 }
--- MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/adapter/RankAdapter.java
+++ MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/adapter/RankAdapter.java
@@ -1,5 +1,8 @@
 package com.example.hfp.MicroCommonweal.adapter;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,46 +10,38 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.hfp.MicroCommonweal.R;
+import com.example.hfp.MicroCommonweal.object.Charity;
 import com.example.hfp.MicroCommonweal.object.Rank;
 
 import java.util.List;
 
-public class RankAdapter extends RecyclerView.Adapter<RankAdapter.ViewHolder>{
+public class RankAdapter extends BaseQuickAdapter<Rank> {
     private List<Rank> mRankList;
-    static  class ViewHolder extends  RecyclerView.ViewHolder{
-        ImageView avatarIamge;
-        TextView rank;
-        TextView donatenumber;
+    private Context context;
 
-        public ViewHolder(View view){
-            super(view);
-            avatarIamge = (ImageView) view.findViewById(R.id.image_touxiangkuang);
-            rank = (TextView)view.findViewById(R.id.str_ranking);
-            donatenumber  = (TextView)view.findViewById(R.id.str_num_bean);
+    public RankAdapter(@LayoutRes int layoutResId, @Nullable List<Rank> data, Context context){
+        super(layoutResId, data);
+        mRankList = data;
+        this.context = context;
+    }
+
+    @Override
+    protected void convert(final BaseViewHolder baseViewHolder, final Rank rank) {
+        baseViewHolder.setText(R.id.str_ranking, rank.getRank());
+        baseViewHolder.setText(R.id.str_num_bean, rank.getDonatenumber());
+        baseViewHolder.setImageResource(R.id.image_touxiangkuang, rank.getAvator());
+
+    }
+
+    public void removeAllData() {
+        while (mRankList != null && !mRankList.isEmpty()) {
+            remove(0);
         }
     }
 
-    public RankAdapter(List<Rank> RankList){
-        mRankList = RankList;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rank_item,parent,false);
-        ViewHolder holder = new ViewHolder(view);
-        return  holder;
-
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder,int position){
-        Rank rank = mRankList.get(position);
-        holder.rank.setText(rank.getRank());
-        holder.avatarIamge.setImageResource(rank.getAvator());
-        holder.donatenumber.setText(rank.getDonatenumber());
-
-    }
     @Override
     public  int getItemCount(){
         return mRankList.size();
