2d776cb4f493128042b4f7e19bd610e69235e189
changhao <1037855969@qq.com>
2018-06-29 15:55:34 +0800

    修改刷新和bug

--- MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/activity/RankActivity.java
+++ MC_FrontEnd/app/src/main/java/com/example/hfp/MicroCommonweal/activity/RankActivity.java
@@ -1,5 +1,7 @@
 package com.example.hfp.MicroCommonweal.activity;
 
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,10 +16,12 @@ import com.example.hfp.MicroCommonweal.object.Rank;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RankActivity extends AppCompatActivity implements View.OnClickListener{
+public class RankActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener,View.OnClickListener{
     private List<Rank> rankList = new ArrayList<>();
     RecyclerView recyclerView;
     private Button button_back;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    RankAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +32,45 @@ public class RankActivity extends AppCompatActivity implements View.OnClickListe
 
 
         initRank();//初始化消息
+        initView();
         //初始化消息列表的recycle和adapter
+//        recyclerView = (RecyclerView)findViewById(R.id.rv_rank);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//        RankAdapter adapter = new RankAdapter(rankList);
+//        recyclerView.setAdapter(adapter);
+    }
+
+    private void initView() {
         recyclerView = (RecyclerView)findViewById(R.id.rv_rank);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        RankAdapter adapter = new RankAdapter(rankList);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        initAdapter();
+        // addHeadView();
+        recyclerView.setAdapter(adapter);
+    }
+    /**
+     * 初始化adapter
+     */
+    private void initAdapter() {
+        adapter = new RankAdapter(rankList);
         recyclerView.setAdapter(adapter);
     }
 
-
+    /**
+     * 刷新listView
+     */
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initAdapter();
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        }, 1000);
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
